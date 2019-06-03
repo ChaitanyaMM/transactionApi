b38b9176ca9fd7674f72f1bf4c80f06822850370
ChaitanyaMM <krishnachaitu.17@gmail.com>
2019-06-03 18:26:35 +0530

    modified application.properties

--- src/main/java/com/transaction/zone/Entities/User.java
+++ src/main/java/com/transaction/zone/Entities/User.java
@@ -1,33 +1,44 @@
 package com.transaction.zone.Entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-
 @Data
 @Entity
 @Table(name = "user")
-
 public class User {
-	
-	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-	@Getter @Setter private Long userId;
-	
-	@Getter @Setter private String userName;
-	
-	@Getter @Setter private String email;
-	 
-	@Getter @Setter private String mobileNumber;
-	
-	@Getter @Setter private String profileName;
-	
-	@Getter @Setter private String password;
-	 
-	
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Getter
+	@Setter
+	private Long userId;
+
+	@Getter
+	@Setter
+	private String userName;
+
+	@Getter
+	@Setter
+	private String email;
+
+	@Getter
+	@Setter
+	private String mobileNumber;
+
+	@Getter
+	@Setter
+	private String profileName;
+
+	@Getter
+	@Setter
+	private String password;
 
 }
